6f8e67e0abe233ffd2ab587f7d06dc2d93702489
BritGirlCoder <deborahtwestwood@gmail.com>
2019-02-27 21:25:53 -0700

    WIP - HelloWorldController

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my <b>default<b> action...";
+        }
+
+        //
+        // GET: /HelloWord/Welcome/
+
+        // if using this: public string Welcome(string name, int numTimes = 1) params are passed as query strings
+        // as written below, ID is a param because it matches the URL specification in the RegisterRoutes method.
+        // we can also add name to the URL specification (see RouteConfig file)
+        public string Welcome(string name, int ID = 1)
+        {
+            // note that HtmlEncode does not accept string interpolation - all methods in the controller will fail.
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+    }
+}
